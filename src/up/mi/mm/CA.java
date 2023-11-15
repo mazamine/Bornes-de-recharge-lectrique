cc6455835f65ec25227fbdb5357960ee04f7a9cd
MERABET Melissa <101985807+Meliissaa6@users.noreply.github.com>
2023-11-15 22:51:54 +0100

    Update CA.java

--- src/up/mi/mm/CA.java
+++ src/up/mi/mm/CA.java
@@ -1,240 +1,152 @@
-package up.mi.mm;
-
-
+package up.mi.mm.projet;
 
 //Cette classe represente la communaute d'aggolomeration
-
 public class CA {
-
 	private int nbSommets;
-
-	private int[][]matrice;//adjacence
-
+	
+	private int[][]matrice;
 	private boolean[] bornesDeRecharge;
-
 	
-
-	public CA(int nbSommets) {
-
-		this.nbSommets=nbSommets;
-		matrice=new int[nbSommets][nbSommets];
-		bornesDeRecharge=new boolean[nbSommets];
+	public CA(int nbSommets) throws InvalidInputException {
+		if(nbSommets>0) {
+			this.nbSommets=nbSommets;
+			matrice=new int[nbSommets][nbSommets];
+			bornesDeRecharge=new boolean[nbSommets];
+		} else {
+			throw new InvalidInputException("Le nombre de villes doit etre supérieur strictement à 0");
+		}
 	}
-
+	
 	public void remplirMatrice() {
-
 		for(int i=0; i<nbSommets;i++) {
-
 			for(int j=0; j<nbSommets;j++) {
-
 				matrice[i][j]=0;
-
 			}
-
 		}
-
 	}
-
 	
-
-	public void ajouterVille() {
-
-		nbSommets++;
-
-		int[][] nouvelleMatrice = new int [nbSommets][nbSommets];
-
-		for(int i=0; i<nbSommets;i++) {
-
-			for(int j=0; j<nbSommets;j++) {
-
-				nouvelleMatrice[i][j]=matrice[i][j];
-
-			}
-
-		}
-
-		matrice=nouvelleMatrice;
-
-	}
-
-	
-
-	public void supprimerVille(int sommet) {
-
-		if(sommet < 0 || sommet >= nbSommets) {
-
-			System.out.println("La ville n'existe pas");
-
-			return;
-
-		}
-
-		//supprimer la ligne du sommet
-
-		for(int i=sommet; i<nbSommets-1; i++) {
-
-			for(int j=0; j<nbSommets;j++) {
-
-				matrice[i][j]=matrice[i+1][j];
-
-			}
-
-		}
-
-		//supprimer la colonne du sommet
-
-		for(int j=sommet; j<nbSommets-1; j++) {
-
-			for(int i=0; i<nbSommets;i++) {
-
-				matrice[i][j]=matrice[i][j+1];
-
-			}
-
-		}
-
-		nbSommets--;
-
-		int [][] nouvelleMatrice=new int [nbSommets][nbSommets];
-
-		for(int i=0;i<nbSommets;i++) {
-
-			for(int j=0;j<nbSommets;j++) {
-
-				nouvelleMatrice[j][j]=matrice[i][j];
-
-			}
-
-		}
-
-		matrice=nouvelleMatrice;
-
-	}
-
-	
-
-	public void ajouterArete(int sommetSource, int sommetCible ) {
-
-		//on verifie si les deux sommets existent déjà dans la matrice
-
-		if(sommetSource < nbSommets && sommetCible < nbSommets) {
-
-			// c'est un graphe non orienté donc la matrice est symétrique
-
-			matrice[sommetSource][sommetCible]=1;
-
-			matrice[sommetCible][sommetSource]=1;
-
-		} else {
-
-			System.out.println("les villes n'existent pas dans la matrice");
-
-		}
-
-	}
-
-	
-
-	public void supprimerArete(int sommetSource, int sommetCible ) {
-
-		if(sommetSource < nbSommets && sommetCible < nbSommets) {
-
-			// c'est un graphe non orienté donc la matrice est symétrique
-
-			matrice[sommetSource][sommetCible]=0;
-
-			matrice[sommetCible][sommetSource]=0;
-
-		} else {
-
-			System.out.println("les villes n'existent pas dans la matrice");
-
-		}
-
-	}
-
-	public void ajouterBorneDeRecharge(int sommet) {
-
-		//on verifie si la ville existe, et si elle n'a pas deja une borne de recharge 
-
-		if ((sommet>=0 || sommet < nbSommets) && !bornesDeRecharge[sommet]) {
-
-	            bornesDeRecharge[sommet] = true;
-	            System.out.println("hello3");
-	    } else {
-
-	    	System.out.println("La ville n'existe pas ou elle a deja une borne de recharge");
-	    	System.out.println(sommet + " " + bornesDeRecharge[sommet]);
-
-	    }
-
-		System.out.println("les villes qui ont des bornes de recharges sont: ");
-
-        for(int i=0; i<nbSommets;i++) {
-
-        	if(bornesDeRecharge[i]==true) {
-
-        		System.out.println(i);
-
-        	}
-
-        }
-
-	}
-
-	public void supprimerBorneRecharge(int sommet) {
-
-		//on verifie si le sommet existe 
-
-        if (sommet>0 || sommet < nbSommets) {
-
-        	//on parcourt les sommets adjacents pour savoir si au moins l'un d'eux a une borne
-
-        	for(int j=0;j<nbSommets;j++) {
-
-        		if(matrice[sommet][j]==1 && bornesDeRecharge[j]==true) {
-
-        			bornesDeRecharge[sommet] = false;
-
-        			return;
-
-        		} 
-
-        	}
-
-        	System.out.println("La ville n'a pas de ville adjacente qui a une borne de recharge");
-
-        } else { 
-
-        	System.out.println("La ville n'existe pas");
-
-        }
-
-        System.out.println("les villes qui ont des bornes de recharges sont: ");
-
-        for(int i=0; i<nbSommets;i++) {
-
-        	if(bornesDeRecharge[i]==true) {
-
-        		System.out.println(i);
-
-        	}
-
-        }
-
-    }
-
 	public void RemplirBornesDeRecharge() {
-
 		for(int i=0;i<nbSommets;i++) {
-
 			bornesDeRecharge[i]=true;
-
 		}
-
 	}
-
-
-
-}
+	
+	public void ajouterVille() {
+		nbSommets++;
+		int[][] nouvelleMatrice = new int [nbSommets][nbSommets];
+		for(int i=0; i<nbSommets;i++) {
+			for(int j=0; j<nbSommets;j++) {
+				nouvelleMatrice[i][j]=matrice[i][j];
+			}
+		}
+		matrice=nouvelleMatrice;
+	}
+	
+	public void supprimerVille(int sommet) throws InvalidInputException {
+		if(sommet < 0 || sommet >= nbSommets) {
+			throw new InvalidInputException("La ville n'existe pas");
+		} else {
+			//supprimer la ligne du sommet
+			for(int i=sommet; i<nbSommets-1; i++) {
+				for(int j=0; j<nbSommets;j++) {
+					matrice[i][j]=matrice[i+1][j];
+				}
+			}
+			//supprimer la colonne du sommet
+			for(int j=sommet; j<nbSommets-1; j++) {
+				for(int i=0; i<nbSommets;i++) {
+					matrice[i][j]=matrice[i][j+1];
+				}
+			}
+			nbSommets--;
+			int [][] nouvelleMatrice=new int [nbSommets][nbSommets];
+			for(int i=0;i<nbSommets;i++) {
+				for(int j=0;j<nbSommets;j++) {
+					nouvelleMatrice[i][j]=matrice[i][j];
+				}
+			}
+			matrice=nouvelleMatrice;
+		}
+	}
+	
+	public void ajouterArete(int sommetSource, int sommetCible ) throws InvalidInputException {
+		//on verifie si les deux sommets existent déjà dans la matrice
+		if((sommetSource<nbSommets) && (sommetSource>=0) && (sommetCible<nbSommets) && (sommetCible>=0)) {
+			if (matrice[sommetSource][sommetCible]==1) {
+				System.out.println("Il existe déjà une route entre la ville "+ sommetSource + " et la ville "+ sommetCible);
+			} else {
+			// c'est un graphe non orienté donc la matrice est symétrique
+				matrice[sommetSource][sommetCible]=1;
+				matrice[sommetCible][sommetSource]=1;
+			}
+		} else {
+			throw new InvalidInputException("les villes n'existent pas");
+		}
+	}
+	
+	public void supprimerArete(int sommetSource, int sommetCible )throws InvalidInputException {
+		if((sommetSource<nbSommets) && (sommetSource>=0) && (sommetCible<nbSommets) && (sommetCible>=0)) {
+			if(matrice[sommetSource][sommetCible]==1) {
+			// c'est un graphe non orienté donc la matrice est symétrique
+				matrice[sommetSource][sommetCible]=0;
+				matrice[sommetCible][sommetSource]=0;
+			} else {
+				System.out.println("Il n'existe pas de route entre la ville "+ sommetSource + " et la ville "+ sommetCible);
+			}
+		} else {
+			throw new InvalidInputException("les villes n'existent pas");
+		}
+	}
+	public void ajouterBorneDeRecharge(int sommet) throws InvalidInputException{
+		//on verifie si la ville existe, et si elle n'a pas deja une borne de recharge 
+		if(sommet < 0 || sommet >= nbSommets) {
+			throw new InvalidInputException("La ville n'existe pas");
+		} else if (bornesDeRecharge[sommet]) {
+			System.out.println("La ville a déjà une borne de recharge");
+	    } else {
+	    	 bornesDeRecharge[sommet] = true;
+	    }
+		System.out.println("les villes qui ont des bornes de recharges sont: ");
+        for(int i=0; i<nbSommets;i++) {
+        	if(bornesDeRecharge[i]==true) {
+        		System.out.print(i+ " ");
+        	}
+        }
+        System.out.println("\n");
+	}
+	
+	public void supprimerBorneRecharge(int sommet)throws InvalidInputException {
+		//on verifie si le sommet existe 
+        if (sommet>=0 || sommet < nbSommets) {
+        	if (!bornesDeRecharge[sommet]) {
+        		System.out.println("La ville n'a pas de borne de recharge");
+        		return;
+        	}
+        	//on parcourt les sommets adjacents pour savoir si au moins l'un d'eux a une borne
+        	else {
+        		for(int j=0;j<nbSommets;j++) {
+	        		if(matrice[sommet][j]==1 && bornesDeRecharge[j]==true) {
+	        			bornesDeRecharge[sommet] = false;
+	        			System.out.println("les villes qui ont des bornes de recharges sont: ");
+	        	        for(int i=0; i<nbSommets;i++) {
+	        	        	if(bornesDeRecharge[i]==true) {
+	        	        		System.out.print(i+ " ");
+	        	        	}
+	        	        }
+	        			return;
+	        		} 
+        		}
+        	}
+        	System.out.println("La ville n'a pas de ville adjacente qui a une borne de recharge");
+        } else { 
+        	throw new InvalidInputException("La ville n'existe pas");
+        }
+        System.out.println("les villes qui ont des bornes de recharges sont: ");
+        for(int i=0; i<nbSommets;i++) {
+        	if(bornesDeRecharge[i]==true) {
+        		System.out.println(i);
+        	}
+        }
+    }
+} 
 
