45f5b0990bb425c844d409020e49a4bbd2774ca3
amine <aminemazouz236@gmail.com>
2023-12-01 15:48:29 +0100

    add class Ville
    created a new class Ville instead of using 2 lists of Strings for the cities and the recharging stations

--- src/projet/Main.java
+++ src/projet/Main.java
@@ -10,37 +10,61 @@ public class Main {
 
 		System.out.println("Bienvenue dans le programme de configuration des zones de recharge.");
 
-		int numberOfCities = 0;
+		/*
+		 * int numberOfCities = 0;
+		 * 
+		 * // Handle InputMismatchException // Handle InputMismatchException and
+		 * validate the input boolean validInput = false; while (!validInput) { try {
+		 * System.out.
+		 * print("Veuillez entrer le nombre de villes (inférieur ou égale à 26) : ");
+		 * numberOfCities = scanner.nextInt();
+		 * 
+		 * // Validate the input if (numberOfCities > 0 && numberOfCities <= 26) {
+		 * validInput = true; } else { System.out.
+		 * println("Entree invalide. Veuillez entrer un nombre entre 1 et 26."); } }
+		 * catch (InputMismatchException e) {
+		 * System.out.println("Entree invalide. Veuillez entrer un nombre entier.");
+		 * scanner.nextLine(); // Clear the input buffer } } scanner.nextLine(); //
+		 * Consume the newline character Community community = new
+		 * Community(numberOfCities);
+		 */
 
-		// Handle InputMismatchException
-		// Handle InputMismatchException and validate the input
-		boolean validInput = false;
-		while (!validInput) {
+		Community community = new Community();
+		int choice = 0;
+		String cityName;
+
+		while (true) {
+			System.out.println("\nMenu principal :");
+			System.out.println("1. Ajouter une Ville");
+			System.out.println("2. Terminer la configuration");
+
 			try {
-				System.out.print("Veuillez entrer le nombre de villes (inférieur ou égale à 26) : ");
-				numberOfCities = scanner.nextInt();
-
-				// Validate the input
-				if (numberOfCities > 0 && numberOfCities <= 26) {
-					validInput = true;
-				} else {
-					System.out.println("Entree invalide. Veuillez entrer un nombre entre 1 et 26.");
-				}
+				choice = scanner.nextInt();
+				scanner.nextLine(); // Consume the newline character
 			} catch (InputMismatchException e) {
 				System.out.println("Entree invalide. Veuillez entrer un nombre entier.");
 				scanner.nextLine(); // Clear the input buffer
+				continue;
 			}
-		}
-		scanner.nextLine(); // Consume the newline character
+			if (choice == 1) {
+				System.out.print("Entrez son nom : ");
+				cityName = scanner.nextLine();
+				community.addVille(cityName);
+			} else if (choice == 2) {
 
-		Community community = new Community(numberOfCities);
+				break;
+			} else {
+				System.out.println("Choix non valide !");
+			}
+
+		}
+
+		choice = 0;
 
 		while (true) {
 			System.out.println("\nMenu principal :");
 			System.out.println("1. Ajouter une route");
 			System.out.println("2. Terminer la configuration");
-
-			int choice = 0;
 
 			// Handle InputMismatchException
 			try {
@@ -56,11 +80,14 @@ public class Main {
 				System.out.print("Veuillez entrer les villes entre lesquelles ajouter une route (par exemple, A B) : ");
 				String input = scanner.nextLine();
 				String[] cities = input.split(" ");
-				community.addRoute(cities[0], cities[1]);
-			} else if (choice == 2) {
+				if (cities.length == 2 && !cities[0].equals(cities[1])) {
+					community.addRoute(cities[0], cities[1]);
+				} else {
+					System.out.println("Entrée invalide. Veuillez entrer deux villes différentes.");
+				}
+			}
+			if (choice == 2) {
 				break;
-			} else {
-				System.out.println("Choix non valide. Veuillez réessayer.");
 			}
 		}
 
@@ -73,7 +100,7 @@ public class Main {
 			System.out.println("2. Retirer une zone de recharge");
 			System.out.println("3. Terminer");
 
-			int choice = 0;
+			choice = 0;
 
 			// Handle InputMismatchException
 			try {
@@ -92,7 +119,7 @@ public class Main {
 			} else if (choice == 2) {
 				System.out.print("Veuillez entrer la ville où retirer une zone de recharge : ");
 				String city = scanner.nextLine();
-				community.removeRechargeZone(city);
+				community.deleteRechargeZone(city);
 			} else if (choice == 3) {
 				break;
 			} else {
